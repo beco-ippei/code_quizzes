4d05e74299cb91531cac23f830e17ffea6a63824
beco-ippei <beco.ippei@gmail.com>
2013-11-12 01:02:25 +0900

    add websocket app

---
+++ code_breaker/beco-ippei/socket_server.rb
@@ -0,0 +1,29 @@
+require 'em-websocket'
+require './lib/breaker'
+$breakers = {}
+
+puts 'start websocket server ..'
+EventMachine::WebSocket.start(host: '0.0.0.0', port: 3011) do |ws|
+  ws.onopen do
+    ans = 4.times.map {|e| rand 10 }.join
+    $breakers[ws.__id__] = Breaker.new(ans)
+    ws.send "msg: server opened & code initialized"
+  end
+
+  ws.onmessage do |msg|
+    puts "received #{msg}"
+    val = msg.split(':')
+
+    case val[0]
+    when 'msg'
+      # do nothing
+    when 'val'
+      result = $breakers[ws.__id__].try val[1]
+      ws.send "status: #{result.join}"
+    end
+  end
+
+  ws.onclose do
+    puts "close socket"
+  end
+end
